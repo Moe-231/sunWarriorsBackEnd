cde0a36feb1d14e679a786ad485196c29253ef51
Moe-231 <cmut0006@student.monash.edu>
2026-03-15 17:44:23 +1000

    Database query test commit

--- routes/postGress.js
+++ routes/postGress.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db')
 
-router.get("/uv", async (req, res) => {
-    clg("Request made to postgress/uv api")
-    const {lat, lon} = req.query
-    clg("User Location is: ", lat, lon)
+router.get("/cancerincidence", async (req, res) => {
     try {
-        const uvResponse = await axios.get(
-            `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY }`
-        )
-
-        res.json(uvResponse.data)
+        const result = await pool.query(`
+           SELECT cancer_type, SUM(incidence_count) as total_cases 
+           FROM cancerincidence
+           GROUP BY cancer_type
+           ORDER BY total_cases DESC
+            `)
+        res.json(result.rows)    
         
     } catch (error) {
-        res.status(500).json({error: "UV fetch failed"})
+        console.error(error)
+        res.status(500).json({ error: "Database query failed"})
         
     }
 })
